ea9a3b9817ff73bb71336e14fb1e4e52a0d8c115
Mannat0105 <mannatprocha0105@gmail.com>
2026-03-28 02:23:58 +0530

    connect frontend to deployed backend

--- dashboard.py
+++ dashboard.py
@@ -1,16 +1,19 @@
-import plotly.express as px
 import streamlit as st
 import requests
 import time
+import plotly.express as px
 
 # Page settings
 st.set_page_config(page_title="System Monitor", layout="wide")
 
 st.title("💻 System Monitoring Dashboard")
 
-# Backend URL
-BASE_URL = "http://127.0.0.1:8000"
+# Sidebar controls
+st.sidebar.title("⚙️ Controls")
+refresh_rate = st.sidebar.slider("Refresh Rate (seconds)", 1, 5, 2)
 
+# Backend URL
+BASE_URL = "https://system-monitoring-tool.onrender.com"
 # Create columns
 col1, col2, col3 = st.columns(3)
 
@@ -28,20 +31,35 @@ try:
     col2.metric("Memory Usage", f"{memory}%")
     col3.metric("Disk Usage", f"{disk}%")
 
+    # 🚨 Alerts
+    if cpu > 80:
+        st.warning("⚠️ High CPU Usage!")
+
+    if memory > 80:
+        st.warning("⚠️ High Memory Usage!")
+
+    if disk > 80:
+        st.warning("⚠️ Disk Almost Full!")
+
+    # 📊 Store history for graph
+    if "cpu_history" not in st.session_state:
+        st.session_state.cpu_history = []
+
+    st.session_state.cpu_history.append(cpu)
+
+    # Keep only last 20 values
+    st.session_state.cpu_history = st.session_state.cpu_history[-20:]
+
+    # 📈 Create graph
+    fig = px.line(st.session_state.cpu_history, title="CPU Usage Trend")
+    st.plotly_chart(fig)
+
+    # 🕒 Timestamp
+    st.write("Last Updated:", data["timestamp"])
+
 except:
     st.error("⚠️ Backend not running!")
-# Store history
-if "cpu_history" not in st.session_state:
-    st.session_state.cpu_history = []
 
-st.session_state.cpu_history.append(cpu)
-
-# Keep last 20 values
-st.session_state.cpu_history = st.session_state.cpu_history[-20:]
-
-# Create graph
-fig = px.line(st.session_state.cpu_history, title="CPU Usage Trend")
-st.plotly_chart(fig)
-# Auto refresh every 2 seconds
-time.sleep(2)
+# Auto refresh
+time.sleep(refresh_rate)
 st.rerun()
